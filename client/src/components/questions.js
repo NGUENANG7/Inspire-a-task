15925b94b290780b20dfda4dae97948ddf5729ef
Joy <joyumoekpo1@gmail.com>
2021-05-10 20:02:25 -0500

    Added QuestionPage.js with async function and questions to question.js file

---
+++ client/src/components/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is your location?',
+        name: 'location',
+    },
+    {
+        type: 'input',
+        message: 'What is your favorite hobby?',
+        name: 'hobby',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your LinkedIn URL?',
+        name: 'linkedin',
+    },
+];
+
+module.exports = questions;
