ffc428d1cb0c9832848996f6748dab94fb154998
Joy <joyumoekpo1@gmail.com>
2021-05-15 14:34:20 -0500

    Added TodoList to TaskPage

--- client/src/pages/TaskPage.js
+++ client/src/pages/TaskPage.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+
 import { useAuth0 } from '@auth0/auth0-react';
 
 import { useNavigate } from 'react-router-dom';
@@ -19,6 +23,7 @@ export const TaskPage = () => {
   return (
     <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-dark drop-shadow-lg text-2xl">
       This is a task page
+      <TodoList />
     </div>
   );
 };
