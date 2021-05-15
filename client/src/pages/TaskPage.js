f254dc2d9aa7122bc75b62910342df76218cb5fb
Joy <joyumoekpo1@gmail.com>
2021-05-15 14:32:08 -0500

    Added TodoList component

--- client/src/pages/TaskPage.js
+++ client/src/pages/TaskPage.js
@@ -2,10 +2,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 import { useNavigate } from 'react-router-dom';
 
-import React, { useState } from 'react';
-import TodoForm from './pages/TodoForm';
-import Todo from './pages/Todo';
-
 export const TaskPage = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   const navigate = useNavigate();
@@ -26,68 +22,3 @@ export const TaskPage = () => {
     </div>
   );
 };
-
-function TodoList() {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = todo => {
-      if (!todo.text || /^\s*$/.test(todo.text)) {
-          return;
-      }
-
-      const newTodos = [todo, ...todos];
-
-      setTodos(newTodos);
-      console.log(...todos);
-  };
-
-  const updateTodo = (todoId, newValue) => {
-      if (!newValue.text || /^\s*$/.test(newValue.text)) {
-          return;
-      }
-
-      setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
-  };
-
-  const removeTodo = id => {
-      const removedArr = [...todos].filter(todo => todo.id !== id);
-
-      setTodos(removedArr);
-  };
-
-  const completeTodo = id => {
-      let updatedTodos = todos.map(todo => {
-          if (todo.id === id) {
-              todo.isComplete = !todo.isComplete;
-          }
-          return todo;
-      });
-      setTodos(updatedTodos);
-  };
-
-  return ( <
-      >
-      <
-      h1 > What are you working on today?</h1> <
-      TodoForm onSubmit = {
-          addTodo
-      }
-      /> <
-      Todo todos = {
-          todos
-      }
-      completeTodo = {
-          completeTodo
-      }
-      removeTodo = {
-          removeTodo
-      }
-      updateTodo = {
-          updateTodo
-      }
-      /> <
-      />
-  );
-}
-
-export default TodoList;
