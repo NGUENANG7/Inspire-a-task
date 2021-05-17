facd3fa0c31ead99b4a1e5e18f0d27e171d571e6
Jas James <jasdjames@gmail.com>
2021-05-17 15:28:19 -0500

    git and post routes working, axios call working

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,71 +1,83 @@
-import React, {
-    useState
-} from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoForm from './TodoForm';
 import Todo from './Todo';
 import '../App.css';
+import Axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export const TodoList = () => {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-    const addTodo = todo => {
-        if (!todo.text || /^\s*$/.test(todo.text)) {
-            return;
-        }
+  useEffect(() => {
+    console.log('FIRST TIME ONLY get notes from DB');
+    Axios.get('http://localhost:8080/tasks').then(function (userData) {
+      console.log('USER FROM DB!!!', userData);
+      setTodos(userData.data.tasks);
+    });
+  }, []);
 
-        const newTodos = [todo, ...todos];
+  useEffect(() => {
+    if (todos.length > 0) {
+      console.log('TODOS have changed!');
+      var sendToBackedn = {
+        todos: todos,
+        userName: user.given_name,
+      };
+      Axios.post('http://localhost:8080/tasks', sendToBackedn).then(function (data) {
+        console.log('we got this back from the BE', data);
+      });
+    }
+  }, [todos]);
 
-        setTodos(newTodos);
-        console.log(...todos);
-    };
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    todo.isComplete = false;
 
-    const updateTodo = (todoId, newValue) => {
-        if (!newValue.text || /^\s*$/.test(newValue.text)) {
-            return;
-        }
+    const newTodos = [todo, ...todos];
 
-        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
-    };
+    setTodos(newTodos);
+  };
 
-    const removeTodo = id => {
-        const removedArr = [...todos].filter(todo => todo.id !== id);
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
 
-        setTodos(removedArr);
-    };
-
-    const completeTodo = id => {
-        let updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                todo.isComplete = !todo.isComplete;
-            }
-            return todo;
-        });
-        setTodos(updatedTodos);
-    };
-
-    return ( <
-        >
-        <
-        h1 > What are you working on today?</h1> <
-        TodoForm onSubmit = {
-            addTodo
-        }
-        /> <
-        Todo todos = {
-            todos
-        }
-        completeTodo = {
-            completeTodo
-        }
-        removeTodo = {
-            removeTodo
-        }
-        updateTodo = {
-            updateTodo
-        }
-        /> <
-        />
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
     );
-}
+  };
+
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+  console.log('Our todo state!!!', todos);
+  return (
+    <>
+      <h1> What are you working on today?</h1> <TodoForm onSubmit={addTodo} />{' '}
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />{' '}
+    </>
+  );
+};
 
 export default TodoList;
--- server.js
+++ server.js
@@ -11,6 +11,46 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/task-app', {
+  // connecting to the mongodb database name: "task-app" locally
+  keepAlive: true, // keeping the connection alive
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set('debug', true); // enabling debugging information to be printed to the console for debugging purposes
+mongoose.Promise = Promise; // setting mongoose's Promise to use Node's Promise
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+require('./routes/index.js')(app);
+
+// const userModel = require('./models/user');
+
+// userModel.create({ name: 'jas', email: 'jas@jas.com', tasks: [], rewards: [] });
+
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get('*', (req, res) => {
