389c284292aefa3766a2da8620f2b0fb6711231f
Joy <joyumoekpo1@gmail.com>
2021-05-19 18:24:42 -0500

    Bgan formating reward page

--- client/src/pages/Reward.js
+++ client/src/pages/Reward.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../App.css';
 
 function Reward() {
     return (
-        <div>
-            <button>Youtube</button> <span>|| </span>
-            <button>Facebook</button> <span>|| </span>
-            <button>Instagram</button> <span>|| </span>
-            <button>Twitter</button> <span>|| </span>
-            <button>Discord</button> <span>|| </span>
-            <button>Insert your own link!</button>            
+        <div class="reward-container icons">
+            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.youtube.com/" target="_blank">Youtube</a></button> <span>|| </span>
+            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.facebook.com/" target="_blank">Facebook</a></button> <span>|| </span>
+            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.instagram.com/" target="_blank">Instagram</a></button> <span>|| </span>
+            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.twitter.com/" target="_blank">Twitter</a></button> <span>|| </span>
+            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.discord.com/" target="_blank">Discord</a></button><span>|| </span>
+            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Insert your own link!</button>            
         </div>
     )
 }
