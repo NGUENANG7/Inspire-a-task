a7f3378aa8b0f88bee119da1619025aedd317f32
Jas James <jasdjames@gmail.com>
2021-05-19 21:03:16 -0500

    Timer added with 10 mins

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Main } from './layouts/Main';
 import { TaskPage, FrontPage } from '../src/pages';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { useAuth0 } from '@auth0/auth0-react';
-import Reward from './pages/Reward';
+import { Reward } from './pages/Reward';
 
 // import { Photo } from './pages/Photo';
 
--- client/src/pages/Reward.js
+++ client/src/pages/Reward.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Timer } from '../components';
 import '../App.css';
-
-function Reward() {
-    return (
-        <div class="reward-container icons">
-            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.youtube.com/" target="_blank">Youtube</a></button> <span>|| </span>
-            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.facebook.com/" target="_blank">Facebook</a></button> <span>|| </span>
-            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.instagram.com/" target="_blank">Instagram</a></button> <span>|| </span>
-            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.twitter.com/" target="_blank">Twitter</a></button> <span>|| </span>
-            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.discord.com/" target="_blank">Discord</a></button><span>|| </span>
-            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Insert your own link!</button>            
-        </div>
-    )
-}
-
-export default Reward;
+export const Reward = () => {
+  return (
+    <>
+      <div class="reward-container icons">
+        <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          <a href="https://www.youtube.com/" target="_blank">
+            Youtube
+          </a>
+        </button>{' '}
+        <span>|| </span>
+        <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          <a href="https://www.facebook.com/" target="_blank">
+            Facebook
+          </a>
+        </button>{' '}
+        <span>|| </span>
+        <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          <a href="https://www.instagram.com/" target="_blank">
+            Instagram
+          </a>
+        </button>{' '}
+        <span>|| </span>
+        <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          <a href="https://www.twitter.com/" target="_blank">
+            Twitter
+          </a>
+        </button>{' '}
+        <span>|| </span>
+        <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          <a href="https://www.discord.com/" target="_blank">
+            Discord
+          </a>
+        </button>
+        <span>|| </span>
+        <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Insert your own link!
+        </button>
+      </div>
+      <h1>Task CountDown - You have a 10 minute break</h1>
+      <div className="inline-block">
+        <Timer />
+      </div>
+    </>
+  );
+};
