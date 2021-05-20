b80b5bbbf622df10f9b1f5dd7ddc1792eafde235
Jas James <jasdjames@gmail.com>
2021-05-20 18:01:56 -0500

    style header - possible option

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,10 @@ export const Header = () => {
   return (
     <div className=" text-center m-4">
       {' '}
-      <h1 className=" m-3 text-center text-8xl"> Inspire a Task</h1>{' '}
+      <h1 className="border-8 border-fuchsia-600 m-3 text-center text-8xl">
+        {' '}
+        Inspire a Task
+      </h1>{' '}
       <p className="display-4">The microtask app that inspires all</p>{' '}
       <Link to="/">
         <span className="lead">Home</span>
