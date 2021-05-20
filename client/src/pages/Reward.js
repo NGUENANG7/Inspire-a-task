d3a7067761bccc793e91483baf91665c68a0af9e
Joy <joyumoekpo1@gmail.com>
2021-05-19 20:37:47 -0500

    Made links into buttons

--- client/src/pages/Reward.js
+++ client/src/pages/Reward.js
@@ -4,12 +4,12 @@ import '../App.css';
 function Reward() {
     return (
         <div class="reward-container icons">
-            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.youtube.com/" target="_blank">Youtube</a></button> <span>|| </span>
-            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.facebook.com/" target="_blank">Facebook</a></button> <span>|| </span>
-            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.instagram.com/" target="_blank">Instagram</a></button> <span>|| </span>
-            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.twitter.com/" target="_blank">Twitter</a></button> <span>|| </span>
-            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.discord.com/" target="_blank">Discord</a></button><span>|| </span>
-            <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Insert your own link!</button>            
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.youtube.com/" target="_blank">Youtube</a></button> <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.facebook.com/" target="_blank">Facebook</a></button> <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.instagram.com/" target="_blank">Instagram</a></button> <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.twitter.com/" target="_blank">Twitter</a></button> <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"><a href="https://www.discord.com/" target="_blank">Discord</a></button><span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Insert your own link!</button>            
         </div>
     )
 }
