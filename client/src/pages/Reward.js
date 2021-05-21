06f6d2f2f7e54c3d410c15f8e0c6adb9dc0a7c6f
Jas James <jasdjames@gmail.com>
2021-05-21 17:41:29 -0500

    timer adjusted on reward page

--- client/src/pages/Reward.js
+++ client/src/pages/Reward.js
@@ -24,53 +24,75 @@ export const Reward = () => {
     isAuthenticated && (
       <>
         <div class="reward-container icons">
-          <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
-              Youtube
-            </a>
-          </button>{' '}
-          <span>|| </span>
-          <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Facebook
-            </a>
-          </button>{' '}
-          <span>|| </span>
-          <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Instagram
-            </a>
-          </button>{' '}
-          <span>|| </span>
-          <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-            <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
-              Twitter
-            </a>
-          </button>{' '}
-          <span>|| </span>
-          <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-            <a href="https://www.discord.com/" target="_blank" rel="noreferrer">
-              Discord
-            </a>
-          </button>
-          <span>|| </span>
-          <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-            <a href="https://www.tiktok.com/" target="_blank" rel="noreferrer">
-              TikTok
-            </a>
-          </button>
-        </div>
-        <h1>Task CountDown - You have a 10 minute break</h1>
-        <div className="inline-block">
-          <Timer />
+          <div className="float-left">
+            <h1>
+              <span className="text-purple-600">Task Countdown</span> - You have
+              a 10 minute break
+            </h1>
+
+            <Timer />
+          </div>
+          <div className="float-right">
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+              <a
+                href="https://www.youtube.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Youtube
+              </a>
+            </button>{' '}
+            <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Facebook
+              </a>
+            </button>{' '}
+            <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Instagram
+              </a>
+            </button>{' '}
+            <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+              <a
+                href="https://www.twitter.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Twitter
+              </a>
+            </button>{' '}
+            <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+              <a
+                href="https://www.discord.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Discord
+              </a>
+            </button>
+            <span>|| </span>
+            <button class="reward-button bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+              <a
+                href="https://www.tiktok.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                TikTok
+              </a>
+            </button>
+          </div>
         </div>
       </>
     )
