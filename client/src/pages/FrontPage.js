e30d38adae262519386163f54ac77fd7c0575d48
Joy <joyumoekpo1@gmail.com>
2021-05-21 14:10:22 -0500

    Centered Frontpage

--- client/src/pages/FrontPage.js
+++ client/src/pages/FrontPage.js
@@ -2,5 +2,5 @@ import { useAuth0 } from '@auth0/auth0-react';
 export const FrontPage = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   console.log('USER on front page!!!', user);
-  return <div>Welcome to Inspire A Task</div>;
+  return <div style={{display: 'flex', justifyContent: 'center'}}>Welcome to Inspire-A-Task!</div>;
 };
